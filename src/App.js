c10d8b9df90b3974302b742470d85175322aee6d
gyusang <monet000414@naver.com>
2023-02-02 19:36:59 +0900

    The END

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 import Box from './component/Box';
+import ComputerBox from './component/ComputerBox';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // 1. 박스 2개 (타이틀, 사진, 결과)
 // 2. 가위 바위 보 버튼 만들기
@@ -30,6 +33,7 @@ function App() {
   const [userSelect,setuserSelect] = useState(null);
   const [computerSelect,setcomputerSelect] = useState(null);
   const [result,setResult]=useState("")
+  const [computerResult,setcomputerResult] = useState("")
 
   const play=(userchoice)=>{ //play 함수
     setuserSelect(choice[userchoice])
@@ -38,6 +42,7 @@ function App() {
     setcomputerSelect(ComputerChoice)
 
     setResult(judgement(choice[userchoice],ComputerChoice))
+    setcomputerResult(computerjudgement(choice[userchoice],ComputerChoice))
   }
 
   const judgement = (user,computer) =>{
@@ -47,6 +52,15 @@ function App() {
     else if(user.name == "Scissors")return computer.name == "Paper"?"Win":"lose"
     else if(user.name == "Paper")return computer.name == "Rock"?"Win":"lose"
   };
+
+  const computerjudgement = (user,computer) =>{
+    if(user.name == computer.name){
+      return "tie"
+    }else if(user.name == "Rock")return computer.name == "Scissors"?"lose":"Win"
+    else if(user.name == "Scissors")return computer.name == "Paper"?"lose":"Win"
+    else if(user.name == "Paper")return computer.name == "Rock"?"lose":"Win"
+  };
+  
 
   const randomChoice = () =>{
     let itemArray = Object.keys(choice); // 객체에 키 값만 뽑아서 배열로 만들어주는 함수.
@@ -60,15 +74,16 @@ function App() {
   <div>
     <div className='main'>
       <Box title="YOU" item={userSelect} result={result}/>
-      <Box title="Computer" item={computerSelect} result={result}/>
+      {/* <Box title="Computer" item={computerSelect} result={result}/> */}
+      <ComputerBox title="Computer" item={computerSelect} result={computerResult} />
     </div>
     <div className='main-button'>
-      <button onClick={()=>play("scissors")}>가위</button> 
-      <button onClick={()=>play("rock")}>바위</button>
-      <button onClick={()=>play("paper")}>보</button>
+      <Button className='btn' variant="outline-warning" onClick={()=>play("scissors")}>가위</Button> 
+      <Button className='btn' variant="outline-warning" onClick={()=>play("rock")}>바위</Button>
+      <Button className='btn' variant="outline-warning" onClick={()=>play("paper")}>보</Button>
     </div>
   </div>
   );
-}
+  }
 
 export default App;
